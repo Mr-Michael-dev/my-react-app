ae8471ed04bc17b778410b405963345e566ca923
Mr-Michael-dev <michael.oyedepo@gmail.com>
2023-08-08 20:11:32 +0100

    my first app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
-
 function App() {
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header className="App-header container">
+        <img src={`https://img.freepik.com/free-vector/branding-identity-corporate-vector-logo-n-design_460848-8723.jpg?w=740&t=st=1691515831~exp=1691516431~hmac=58e0778687817676cd0d67a898c099f4c2baddbda3e12a8f9ef7603dd4f8898a`} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         This is my first React App
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://mydayplanner.netlify.app/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Plan Your Day Here
         </a>
       </header>
+     
     </div>
+     
   );
 }
 
---
+++ src/main.js
@@ -0,0 +1,21 @@
+import "./App.css";
+
+function MainBody() {
+  return (
+    <div className = "mainBody">
+    <div className = "container welcome">
+      <div>
+        <h1>Welcome to The Grace Place</h1>
+      </div>
+      <div>
+        <p>
+          Where everyone ministers and everyone is ministered to.
+          <em>We are glad you are here.</em>Our expectation is your growth.
+        </p>
+      </div>
+    </div>
+    </div>
+  );
+}
+
+export default MainBody;
