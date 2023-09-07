7d9cfdf7a64a9131be4693a7927455489d2a53b9
Mr-Michael-dev <michael.oyedepo@gmail.com>
2023-09-07 19:26:20 +0100

    create quiz components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
-import './App.css';
-function App() {
-  return (
-    
-    <div className="App">
-      <header className="App-header container">
-        <img src={`https://img.freepik.com/free-vector/branding-identity-corporate-vector-logo-n-design_460848-8723.jpg?w=740&t=st=1691515831~exp=1691516431~hmac=58e0778687817676cd0d67a898c099f4c2baddbda3e12a8f9ef7603dd4f8898a`} className="App-logo" alt="logo" />
-        <p>
-         This is my first React App
-        </p>
-        <a
-          className="App-link"
-          href="https://mydayplanner.netlify.app/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Plan Your Day Here
-        </a>
-      </header>
-     
-    </div>
-     
-  );
+import React, { Component } from 'react'
+import './App.css'
+import Quiz from './Quiz.js'
+
+class App extends Component {
+  render() {
+    return (
+      <Quiz />
+    )
+  }
 }
 
-export default App;
+export default App
