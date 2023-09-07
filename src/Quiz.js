4f1f791c63ad78042caf722ce695bdb42b25e640
Mr-Michael-dev <michael.oyedepo@gmail.com>
2023-09-07 21:03:17 +0100

    create QuizEnd component

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,26 +1,33 @@
-import React, { Component } from 'react'
-import QuizQuestion from './QuizQuestion'
-import QuizEnd from './QuizEnd.js'
+import React, { Component } from "react";
+import QuizQuestion from "./QuizQuestion";
+import QuizEnd from "./QuizEnd.js";
 
-let quizData = require ('./quiz_data.json')
+let quizData = require("./quiz_data.json");
 
 class Quiz extends Component {
   constructor(props) {
-      super(props);
-      this.state = {quiz_position: 1}
-    }
-   
+    super(props);
+    this.state = { quiz_position: 1 };
+  }
+
   render() {
-    const isQuizEnd = true
-    // (this.state.quiz_position - 1 === quizData.quiz_questions.length);
-  return (
-     <div>
-    {isQuizEnd ?
-    <QuizEnd /> :
-    <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/>};
-    </div>
-      );
+    const isQuizEnd =
+      this.state.quiz_position - 1 === quizData.quiz_questions.length;
+    return (
+      <div>
+        {isQuizEnd ? (
+          <QuizEnd />
+        ) : (
+          <QuizQuestion
+            quiz_question={
+              quizData.quiz_questions[this.state.quiz_position - 1]
+            }
+          />
+        )}
+        ;
+      </div>
+    );
   }
 }
 
-export default Quiz
+export default Quiz;
--- src/QuizEnd.js
+++ src/QuizEnd.js
@@ -1,14 +1,14 @@
-import React, {Component} from 'react'
+import React, { Component } from "react";
 
 class QuizEnd extends Component {
-  render () {
+  render() {
     return (
-     <div> 
-        <p>Thanks for playing!</p> 
-        <a href=''>Reset Quiz</a> 
+      <div>
+        <p>Thanks for playing!</p>
+        <a href="">Reset Quiz</a>
       </div>
-    )
+    );
   }
 }
 
-export default QuizEnd
+export default QuizEnd;
